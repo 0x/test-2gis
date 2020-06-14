df675d856ea067b40a2212ad9d17dbda1192c3f6
quip95 <koynov95@gmail.com>
2020-06-14 11:30:07 +0700

    Remove build

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/koynov/Documents/test-2gis
-# Build directory: /Users/koynov/Documents/test-2gis/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googletest")
-subdirs("test")
--- build/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/koynov/Documents/test-2gis/test
-# Build directory: /Users/koynov/Documents/test-2gis/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test2gis_test "/Users/koynov/Documents/test-2gis/build/test/test2gis_test")
-set_tests_properties(test2gis_test PROPERTIES  _BACKTRACE_TRIPLES "/Users/koynov/Documents/test-2gis/test/CMakeLists.txt;21;add_test;/Users/koynov/Documents/test-2gis/test/CMakeLists.txt;0;")
